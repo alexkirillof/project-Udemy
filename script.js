05b69c59a26666e18c03d8435037cc0fc9bb3299
AlexK <alexkirillof@yandex.ru>
2022-07-27 22:19:02 +0300

    second task

--- script.js
+++ script.js
@@ -1,7 +1,15 @@
-const mumberOfFilms = +prompt('Сколько фильмов вы уже посмотрели?', '');
+const numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели?', '');
+
+if (numberOfFilms > 0 && numberOfFilms < 10) {
+    alert("Просмотренно довольно мало фильмов")
+} else if (numberOfFilms >= 10 && numberOfFilms < 30) {
+    alert("Вы классный зритель!")
+} else if (numberOfFilms >= 30) {
+    alert("Вы классный зритель!")
+} else alert("Произошла ошибка!");
 
 const personalMovieDB = {
-    count: mumberOfFilms,
+    count: numberOfFilms,
     movies: {},
     actors: {},
     genres: [],
@@ -9,10 +17,22 @@ const personalMovieDB = {
 }
 
 
-const lastFilm = prompt('Один из последних просмотренных фильмов?', ''),
-    filmRang = prompt('На сколько его оцените?', '');
+const lastFilm = [],
+    filmRang = [];
 
+for (let i = 0; i < 4; i++) {
 
-personalMovieDB['movies'][lastFilm] = filmRang;
+    lastFilm[i] = prompt('Один из последних просмотренных фильмов?', '');
+    while (lastFilm[i] === '' || lastFilm[i] === null || lastFilm[i].length > 50) {
+        lastFilm[i] = prompt('Один из последних просмотренных фильмов?', '');
+    }
+    filmRang[i] = prompt('На сколько его оцените?', '');
+    while (filmRang[i] === '' || filmRang[i] === null) {
+        filmRang[i] = prompt('На сколько его оцените?', '');
+    }
+    personalMovieDB.movies[lastFilm[i]] = filmRang[i]
+
+}
+
 
 console.log(personalMovieDB);
